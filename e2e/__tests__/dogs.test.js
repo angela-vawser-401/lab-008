b5672ae13e3ca27c6ff2ac0c660830f9834868f4
Angela Vawser <amvawser@gmail.com>
2019-09-25 15:05:29 -0700

    deletes a dog

--- e2e/__tests__/dogs.test.js
+++ e2e/__tests__/dogs.test.js
@@ -79,7 +79,7 @@ describe('dogs api', () => {
       });
   });
 
-  it.skip('deletes a dog', () => {
+  it('deletes a dog', () => {
     return postDog(dogData)
       .then(dog => {
         return request
